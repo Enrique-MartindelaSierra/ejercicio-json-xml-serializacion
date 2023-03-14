4e46c27d9d1de19bf37b626a1ebe389d6c29b44b
Enrique-MartindelaSierra <enriquemdlse22@gmail.com>
2023-03-14 22:43:17 +0100

    Initial commit

---
+++ src/main/java/com/enrique/ejercicio_json_xml_serializacion/App.java
@@ -0,0 +1,13 @@
+package com.enrique.ejercicio_json_xml_serializacion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/enrique/ejercicio_json_xml_serializacion/AppTest.java
@@ -0,0 +1,20 @@
+package com.enrique.ejercicio_json_xml_serializacion;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
